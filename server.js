08f0b73dbf81c046cc52ed1e9b9e73b012586e56
rhodinemma <rhodinemma10@gmail.com>
2023-06-19 15:44:47 +0300

    feat: add nodemon and morgan to log API requests

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
 
 app.get("/", (req, res) => {
   res.send("Our API");
 });
 
-app.listen(8000, () => {
-  console.log("Server running on port 8000");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
