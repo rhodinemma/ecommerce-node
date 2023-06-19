2b0901662579cc7da4fb4f81921ceaa11cbd7dc6
rhodinemma <rhodinemma10@gmail.com>
2023-06-19 22:04:42 +0300

    setup validation layer logic

--- config/database.js
+++ config/database.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
 const databaseConnection = () => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then((conn) => {
-      console.log(`Database Connected: ${conn.connection.host}`);
-    })
-    .catch((err) => {
-      console.error(`Database Error: ${err}`);
-      process.exit(1);
-    });
+  mongoose.connect(process.env.DB_URI).then((conn) => {
+    console.log(`Database Connected: ${conn.connection.host}`);
+  });
 };
 
 module.exports = databaseConnection;
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -6,13 +6,26 @@ const {
   updateCategory,
   deleteCategory,
 } = require("../services/categoryService");
+const { param, validationResult } = require("express-validator");
 
 const router = express.Router();
 
 router.route("/").get(getCategories).post(createCategory);
 router
   .route("/:id")
-  .get(getCategory)
+  .get(
+    // 1 - rules
+    param("id").isMongoId().withMessage("Invalid category ID"),
+    // 2 - middleware => catch errors from rules if exist
+    (req, res) => {
+      // finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.any() });
+      }
+    },
+    getCategory
+  )
   .put(updateCategory)
   .delete(deleteCategory);
 
