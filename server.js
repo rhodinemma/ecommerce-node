93ba0499b6f621500fb8d5143b2ea9e445941512
rhodinemma <rhodinemma10@gmail.com>
2023-06-19 15:30:27 +0300

    chore: add base files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Our API");
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
