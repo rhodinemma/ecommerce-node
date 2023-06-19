bdc484ee85a6f4308d9b5e8634b7de83d5d060c3
rhodinemma <rhodinemma10@gmail.com>
2023-06-19 20:52:25 +0300

    chore: refactor global error

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const databaseConnection = require("./config/database");
 const categoryRoute = require("./routes/categoryRoute");
 const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
 
 dotenv.config({ path: "config.env" });
 // connect with database
@@ -31,16 +32,7 @@ app.all("*", (req, res, next) => {
 });
 
 // global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-});
+app.use(globalError);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
