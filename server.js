922130f45b2e13f10b5ec70e7464d48f24027420
rhodinemma <rhodinemma10@gmail.com>
2023-06-19 20:41:22 +0300

    feat: reusable error class

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const databaseConnection = require("./config/database");
 const categoryRoute = require("./routes/categoryRoute");
+const ApiError = require("./utils/apiError");
 
 dotenv.config({ path: "config.env" });
 // connect with database
@@ -24,13 +25,21 @@ app.use("/api/v1/categories", categoryRoute);
 
 app.all("*", (req, res, next) => {
   // create error and send it to error handling middleware
-  const err = new Error(`Can't find this route: ${req.originalUrl}`);
-  next(err.message);
+  // const err = new Error(`Can't find this route: ${req.originalUrl}`);
+  // next(err.message);
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
 // global error handling middleware
 app.use((err, req, res, next) => {
-  res.status(500).json({ err });
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 const PORT = process.env.PORT;
---
+++ utils/apiError.js
@@ -0,0 +1,11 @@
+// @desc    this class is reponsible about operation errors(predictable)
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+
+module.exports = ApiError;
