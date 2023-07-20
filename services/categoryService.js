f799da2a2e84ab9cfc72c39ad6d5e8078f6d45a4
rhodinemma <rhodinemma10@gmail.com>
2023-07-20 10:37:13 +0300

    feat: using multer storage configuration

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,5 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 const Category = require("../models/category");
 const factory = require("./handlersFactory");
+
+// 1- DiskStorage
+const multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/categories");
+  },
+  filename: function (req, file, cb) {
+    // category-${id}-Date.now().jpeg
+    const ext = file.mimetype.split("/")[1];
+    const filename = `category-${uuidv4()}-${Date.now()}.${ext}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: multerStorage });
+
+exports.uploadCategoryImage = upload.single("image");
 
 // @desc    Get list of categories
 // @route   GET /api/v1/categories
